a2e4a99e7e1564f98e667f751ed9f8e9d3872c1d
urmilshroff <urmilshroff@gmail.com>
2018-08-09 09:42:38 +0530

    Created disctionary

--- process_scheduling.py
+++ process_scheduling.py
@@ -15,10 +15,14 @@ class Process(object):
             
 
 processes=[]
+algos=(1="FCFS", 2="SJF", 3="SRTF", 4="Round Robin", 5="Priority")
+
 for p in range(int(input("Enter number of processes:\n"))):
     processes.append(Process(p))
     
-for obj in processes:
-    print("Priority of process {} is {}".format(obj,obj.get_pr()))
-    print("Priority of process {} is {}".format(obj,obj.get_bt()))
-    print("Priority of process {} is {}".format(obj,obj.get_at()))
+# for obj in processes:
+#     print("Priority of process {} is {}".format(obj,obj.get_pr()))
+#     print("Priority of process {} is {}".format(obj,obj.get_bt()))
+#     print("Priority of process {} is {}".format(obj,obj.get_at()))
+
+choice=int(input("Which scheduling algorithm do you want to use?\n1. FCFS\n2. SJF\n3. SRTF \n4. Round Robin\n5. Priority\n"))
