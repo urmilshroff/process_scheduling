2ffac620db0a7da5e3c072cf3facd5b8ad35a42c
urmilshroff <urmilshroff@gmail.com>
2018-08-10 15:44:24 +0530

    Updated

--- process_scheduling.py
+++ process_scheduling.py
@@ -1,11 +1,11 @@
 class Process(object):
     def __init__(self,p_num):
-        self.pr=int(input("Enter Priority of process {}:\n".format(p_num)))
-        self.bt=int(input("Enter Burst Time of process {}:\n".format(p_num)))
-        self.at=int(input("Enter Arrival Time of process {}:\n".format(p_num)))
+        # self.pr=int(input("Enter Priority of process {}:\n".format(p_num)))
+        self.bt=int(input("Enter Burst Time of process {}:\n".format(chr(p_num+65))))
+        self.at=int(input("Enter Arrival Time of process {}:\n".format(chr(p_num+65))))
         
-    def get_pr(self):
-        return self.pr
+    # def get_pr(self):
+    #     return self.pr
         
     def get_bt(self):
         return self.bt
@@ -13,14 +13,15 @@ class Process(object):
     def get_at(self):
         return self.at
 
-def gandt_chart_create(processes):
-    
-    gandt_chart_timestamp=[]
-    
-    for obj in processes:
-        gandt_chart_timestamp.append(obj.get_bt)
+# def gandt_chart_create(processes):
+# 
+#     gandt_chart_timestamp=[] #list which contains burst time of every Process objects
+# 
+#     for obj in processes:
+#         gandt_chart_timestamp.append(obj.get_bt)
+#     print("Times are {}".format(gandt_chart_timestamp))
 
-processes=[] #list which will contain Process class objects
+processes=[] #list which contains Process class objects
 
 for p in range(int(input("Enter number of processes:\n"))):
     processes.append(Process(p)) #appends each object
@@ -32,5 +33,15 @@ choice=int(input("Which scheduling algorithm do you want to use?\n1. FCFS\n2. SJ
 #     print("Priority of process {} is {}".format(obj,obj.get_bt()))
 #     print("Priority of process {} is {}".format(obj,obj.get_at()))
 
-gandt_chart_create(processes)
-
+if choice==1:
+    fcfs(processes)
+elif choice==2:
+    sjf(processes)
+elif choice==3:
+    srtf(processes)
+elif choice==4:
+    roro(processes)
+elif choice==5:
+    prio(processes)
+else:
+    print("Invalid input!")
