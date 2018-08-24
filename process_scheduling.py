c45b3ce7be36f78d0ea89210f4c1b9809e3aca30
urmilshroff <urmilshroff@gmail.com>
2018-08-25 01:21:47 +0530

    FCFS works

--- process_scheduling.py
+++ process_scheduling.py
@@ -1,38 +1,30 @@
 class Process(object):
     def __init__(self,p_num):
-        # self.pr=int(input("Enter Priority of process {}:\n".format(p_num)))
         self.bt=int(input("Enter Burst Time of process {}:\n".format(chr(p_num+65))))
         self.at=int(input("Enter Arrival Time of process {}:\n".format(chr(p_num+65))))
-        
-    # def get_pr(self):
-    #     return self.pr
-        
+        #self.pr=int(input("Enter Priority of process {}:\n".format(chr(p_num+65))))
+
     def get_bt(self):
         return self.bt
         
     def get_at(self):
         return self.at
-
-# def gandt_chart_create(processes):
-# 
-#     gandt_chart_timestamp=[] #list which contains burst time of every Process objects
-# 
-#     for obj in processes:
-#         gandt_chart_timestamp.append(obj.get_bt)
-#     print("Times are {}".format(gandt_chart_timestamp))
+        
+    def get_pr(self):
+        return self.pr
 
 def fcfs(processes):
     p_num,wt,tot_wt,tat=65,0,0,0
-    print("\n")
+    print()
     
     for obj in processes:
-        print("Waiting Time for Process {} is {}ms".format(chr(p_num),wt))
-        # print("Turnaround Time for Process {} is {}ms".format(chr(p_num)))
-        wt=wt+obj.get_bt()-obj.get_at()
+        wt=wt-obj.get_at()
         tot_wt+=wt
+        print("Waiting Time for Process {} is {}ms".format(chr(p_num),wt))
+        wt+=obj.get_bt()+obj.get_at() #adds BT of current process to increase WT of next process
         p_num+=1
         
-    print("Average Waiting Time is {}ms".format(tot_wt/len(processes)))
+    print("\nAverage Waiting Time is {}ms".format(tot_wt/len(processes)))
 
 processes=[] #list which contains Process class objects
 
@@ -40,11 +32,6 @@ for p in range(int(input("Enter number of processes:\n"))):
     processes.append(Process(p)) #appends each object
     
 choice=int(input("Which scheduling algorithm do you want to use?\n1. FCFS\n2. SJF\n3. SRTF \n4. Round Robin\n5. Priority\n"))
-
-# for obj in processes:
-#     print("Priority of process {} is {}".format(obj,obj.get_pr()))
-#     print("Priority of process {} is {}".format(obj,obj.get_bt()))
-#     print("Priority of process {} is {}".format(obj,obj.get_at()))
 
 if choice==1:
     fcfs(processes)
